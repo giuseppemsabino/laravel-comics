57d82e123d3add4069f2798aff898a2951bca08b
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-04-02 20:23:03 +0200

    style

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,9 +1,12 @@
 
 
 <div class="card">
-  {{ $img }}
-  <div class="card-body">
-    <h5 class="card-title">{{$title}}</h5>
+  <div class="img-card-container">
+
+    <img src={{$img}} class="card-img " alt="...">
+  </div>
+  <div class="card-body text-justify">
+    <strong class="card-title">{{$title}}</strong>
     <p class="card-text">{{$price}}</p>
   </div>
 </div>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,18 +6,21 @@ $comics = config('comics');
 ?>
 
 @section('content')
-<h1>homepage</h1>
-<div class="row row-cols-3 g-3">
-    <div class="col">
-
-        
+<section class="jumbo">
+    
+    <button class="btn btm-primary"></button>
+</section>
+<div class="container">
+    <h1>homepage</h1>
+    <div class=""></div>
+    <div class="row row-cols-5 g-3">     
         @foreach($comics as $comic)
         <x-card>
             <x-slot:img>
-                <img src="{{$comic['thumb']}}" class="card-img-top img-fluid w-25" alt="...">
+                {{$comic['thumb']}}
             </x-slot:img>
             <x-slot:title>
-                {{$comic['title']}}
+                {{$comic['series']}}
             </x-slot:title>
             <x-slot:price>
                 {{$comic['price']}}
