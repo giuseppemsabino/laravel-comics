d9ba5f3e9e0a12b9325aa10e6faf7f1b2440cbb6
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-04-03 21:10:24 +0200

    random header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,25 @@
 
 <header>
 
-hader della pagina 
+<div class="navbar-section container ">
+    <div class="navbar">
+        <div class="logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo">
+        </div>
+        <div class="nav-links">
+            <ul class="">
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                
+            </ul>
+
+        
+        </div>
+
+    </div>
+
+
+</div>
 
 </header>
