facd34de046c2c89c08d575824ac60e044289d1c
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-04-04 16:37:02 +0200

    routes nav bar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,13 +4,13 @@
 <div class="navbar-section container ">
     <div class="navbar">
         <div class="logo">
-            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo">
+           <a href="{{url('/')}}"><img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo"></a>
         </div>
         <div class="nav-links">
             <ul class="">
-                <li><a href="#">CHARACTERS</a></li>
-                <li><a href="#">COMICS</a></li>
-                <li><a href="#">MOVIES</a></li>
+                <li><a href="{{url('/characters')}}">CHARACTERS</a></li>
+                <li><a href="{{url('/comics')}}">COMICS</a></li>
+                <li><a href="{{url('/movies')}}">MOVIES</a></li>
                 
             </ul>
 
